748a3dd902520d7902282b52bb29d5dc85f87cad
diegol <diegolopezcdm@gmail.com>
2020-11-09 22:53:09 -0500

    removed unnecessary lines

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,7 +1,4 @@
-//export const HOST_BACKEND = `http://localhost:4200`
 export const HOST_BACKEND = `https://api.echamika.site`
-//export const HOST_BACKEND = `http://loadbalancer-diego-96899420.us-east-2.elb.amazonaws.com`
-//export const HOST_BACKEND = `https://x4omg2wqt9.execute-api.us-east-2.amazonaws.com/prod`
 
 export const TIME_UPDATE_GEOLOCALIZATION = 60000;
 export const RADIO = 0.029;
